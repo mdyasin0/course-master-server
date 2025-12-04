41ef18fc978ed6f27ab8b365d93680497cc816dd
MD Yasin <mdyasin48902@gmail.com>
2025-12-04 23:10:19 +0600

    change comment for deploy

--- index.js
+++ index.js
@@ -576,5 +576,5 @@ app.get("/user", async (req, res) => {
 // ----------------------
 // SERVER START
 // ----------------------
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
